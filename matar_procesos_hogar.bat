992f791fbdbac1147814c32799f33bfaad01f333
braianlopez <braian.lopez@groupcosbpo.com>
2026-07-29 12:22:15 -0500

    feat: visualizacion de fecha/hora de actualizacion en HTML y scripts de limpieza de MySQL

--- matar_procesos_hogar.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-echo ========================================================
-echo   MATANDO PROCESOS Y CONSULTAS BLOQUEANTES EN HOGAR
-echo ========================================================
-python matar_conexiones_hogar.py
-echo.
-pause
